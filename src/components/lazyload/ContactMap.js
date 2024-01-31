bdabe2af78a0cd04dd4c8c8f162aa141549b1d65
Surya <surya@emqube.com>
2024-01-31 12:44:05 +0530

    Changes the contact url

--- src/components/lazyload/ContactMap.js
+++ src/components/lazyload/ContactMap.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Map({ pageAcf }) {
     return (
         pageAcf && pageAcf.mapHtml && (
-            <section className="innerspace-google-map">
+            <section className="innerspace-google-map" id="visit-showroom">
                 <span dangerouslySetInnerHTML={{ __html: pageAcf.mapHtml }} />
             </section>
         )
